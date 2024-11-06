8a901040707231ec24f3c9f1a6ade5c35b82130b
Sajid-1101 <sajidsaleem1101@gmail.com>
2024-11-06 22:20:50 +0530

    New

--- decreasing.c
+++ decreasing.c
@@ -3,11 +3,10 @@ void greeting(int x){
     if(x==0) return ;
     printf("%d\n",x); 
     greeting(x-1);
-    return ;
 }
 int main(){
     int n ;
-    printf("Enter the n : ");
+    printf("Enter the number : ");
     scanf("%d",&n);
     greeting(n);
     return 0;
